d2f62480d6837bb54766bc21a813decac39b4af0
Luiz Carlos Oliveira da Silva <45966334+luelsilva@users.noreply.github.com>
2026-03-04 08:00:35 -0300

    Update API_URL to use production

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 
 
-export const API_URL = 'http://174.138.81.138:3000/api';
-//export const API_URL = 'https://urso-estagio.vercel.app/api';
+//export const API_URL = 'http://174.138.81.138:3000/api';
+export const API_URL = 'https://urso-estagio.vercel.app/api';
 
